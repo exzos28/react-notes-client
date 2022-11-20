4e3d7dba3fcb4dccc3e72aa5777fae9b938279bc
exzos <oleksandr.kurinnyi.dev@gmail.com>
2022-11-20 13:21:32 +0100

    Feature: Format date

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -23,12 +23,13 @@ export default function NoteList({data}: NoteListProps) {
                 const candidateText = elements.join(', ');
                 const text = candidateText.length === 0 ? '-' : candidateText;
                 const croppedText = text.length > 500 ? text.substring(0, 500) + "..." : text;
+                const date = `${new Date(_.creationDate).toLocaleDateString()} ${new Date(_.creationDate).toLocaleTimeString()}`
                 return (
                     <ListItem key={_.id}>
                         <ListItemButton onClick={() => navigate(`notes/${_.id}`)}>
                             <ListItemText
                                 primary={croppedText}
-                                secondary={_.creationDate}
+                                secondary={date}
                             />
                         </ListItemButton>
                     </ListItem>
